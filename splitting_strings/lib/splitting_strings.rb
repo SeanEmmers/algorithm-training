69fabe09bd8406afd18853a0ca08c08b8297472a
Sean Phillips <seanphillips9696@gmail.com>
2021-09-01 15:21:53 +0200

    SRP

--- splitting_strings/lib/splitting_strings.rb
+++ splitting_strings/lib/splitting_strings.rb
@@ -2,7 +2,14 @@ def splitting_strings(str)
 
   str_arr = str.chars.each_slice(2).map(&:join)
 
+  underscore_needed?(str_arr)
+  
+end
+
+def underscore_needed?(str_arr)
+
   str_arr[-1] += '_' if str_arr[-1].length == 1
-    
+
   str_arr
+
 end
