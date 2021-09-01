511cf4251fde69dbd3440aa47f6eda3a6fa62aab
Sean Phillips <seanphillips9696@gmail.com>
2021-09-01 15:26:58 +0200

    testing for new underscorre_needed function

--- splitting_strings/spec/splitting_string_spec.rb
+++ splitting_strings/spec/splitting_string_spec.rb
@@ -1,6 +1,7 @@
 require 'splitting_strings'
 
 describe '.splitting_strings' do 
+
   it 'splits an even string' do
     expect(splitting_strings('abcd')).to eq ['ab', 'cd']
   end
@@ -9,8 +10,20 @@ describe '.splitting_strings' do
     expect(splitting_strings('abcdefghijklmnop')).to eq ['ab', 'cd', 'ef', 'gh', 'ij', 'kl', 'mn', 'op']
   end
 
-  it 'adds an underscore to an odd string' do
+  it 'splits an odd string' do
     expect(splitting_strings('abcde')).to eq ['ab', 'cd', 'e_']
+  end
+
+end
+
+describe '.underscore_needed?' do
+
+  it 'adds an underscore to an odd array' do
+    expect(underscore_needed?(['ab', 'c'])).to eq ['ab', 'c_']
+  end
+  
+  it 'does not add an underscore to an even array' do
+    expect(underscore_needed?(['ab', 'cd'])).to eq ['ab', 'cd']
   end
 
 end
