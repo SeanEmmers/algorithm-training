af8a25772ffb5db372a6d5c5e0448156013c6bbf
Sean Phillips <seanphillips9696@gmail.com>
2021-09-08 16:23:54 +0200

    second test

--- valid_brackets/lib/valid_brackets.rb
+++ valid_brackets/lib/valid_brackets.rb
@@ -1,5 +1,14 @@
 def valid_brackets(brackets)
 
+  left_bracket = 0
+  right_bracket = 0
+
+  brackets.each_char do |bracket|
+    left_bracket += 1 if bracket == '('
+    right_bracket += 1 if bracket == ')'
+  end
+
+  left_bracket == right_bracket
 
 
 end
--- valid_brackets/spec/valid_brackets_spec.rb
+++ valid_brackets/spec/valid_brackets_spec.rb
@@ -1,9 +1,13 @@
 require 'valid_brackets'
 
 describe '.valid_brackets' do
-  
-  it 'returns true for ()' do
+
+  it 'returns true for single bracket' do
     expect(valid_brackets('()')).to eq true
+  end
+
+  it 'returns false for one wrong bracket' do
+    expect(valid_brackets('())')).to eq false
   end
 
 end
