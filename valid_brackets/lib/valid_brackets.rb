271907561a6f13f4a5dbea9ac1b23e6da2e7c002
Sean Phillips <seanphillips9696@gmail.com>
2021-09-11 11:25:29 +0200

    passing third test

--- valid_brackets/lib/valid_brackets.rb
+++ valid_brackets/lib/valid_brackets.rb
@@ -9,9 +9,7 @@ def valid_brackets(brackets)
     right_bracket += 1 if bracket == ')'
   end
 
-  p brackets[0]
-  p brackets[-1]
-  enclosed == true if brackets[0] == '(' && brackets[-1] == ')'
+  enclosed = true if brackets[0] == "(" && brackets[-1] == ")"
 
   left_bracket == right_bracket && enclosed == true
 
