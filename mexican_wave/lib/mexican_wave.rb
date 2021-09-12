90bd077e02260fe013503c2cc1837e5e521dc029
Sean Phillips <seanphillips9696@gmail.com>
2021-09-12 11:42:30 +0200

    failing first test

---
+++ mexican_wave/lib/mexican_wave.rb
@@ -0,0 +1,7 @@
+def mexican_wave(string)
+
+  letter_to_capitilize = 
+
+  string.capitalize
+
+end
--- mexican_wave/spec/mexican_wave_spec.rb
+++ mexican_wave/spec/mexican_wave_spec.rb
@@ -2,7 +2,7 @@ require 'mexican_wave'
 
 describe '.mexican_wave' do
   it 'returns the first word capitalized in an Array' do
-    expect(mexican_wave('hello')).to include ['Hello']
+    expect(mexican_wave('hello')).to eq ['Hello']
   end
 
 
