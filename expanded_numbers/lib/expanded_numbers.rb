69d7778aff3c3db4c63d55051a36db98744236bf
Sean Phillips <seanphillips9696@gmail.com>
2021-09-13 10:00:57 +0200

    challenge passed

--- expanded_numbers/lib/expanded_numbers.rb
+++ expanded_numbers/lib/expanded_numbers.rb
@@ -1,5 +1,13 @@
-def expanded_numbers(num)
+def expanded_numbers(number)
 
-  num.to_s
+  num_arr = number.to_s.split('')
+  arr_num = num_arr.length - 1
+  final_arr = []
+  
+  num_arr.each { |num|
+    final_arr << num + '0' * arr_num if num != '0'
+    arr_num -= 1
+  }
 
+  final_arr.join(' + ')
 end
--- expanded_numbers/spec/expanded_numbers_spec.rb
+++ expanded_numbers/spec/expanded_numbers_spec.rb
@@ -5,5 +5,13 @@ describe '.expanded_numbers' do
     expect(expanded_numbers(11)).to eq '10 + 1'
   end
 
+  it 'doesnt return zeros expanded' do
+    expect(expanded_numbers(101)).to eq '100 + 1'
+  end
+
+  it 'works for a longer number' do
+    expect(expanded_numbers(70304)).to eq '70000 + 300 + 4'
+  end
+
 
 end
