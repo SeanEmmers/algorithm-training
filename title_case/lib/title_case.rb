82eaa1bf771e672ce501f28c50d143e675959783
Sean Phillips <seanphillips9696@gmail.com>
2021-09-16 12:21:29 +0200

    passing on codewars

--- title_case/lib/title_case.rb
+++ title_case/lib/title_case.rb
@@ -1,5 +1,17 @@
-def title_case(title, exceptions)
-  title.capitalize
+def title_case(title, exceptions = '')
 
+  finished = []
+
+  # title.capitalize.split.each { |word| 
+  #   exceptions.downcase.include?(word) ? finished << word : finished << word.capitalize 
+  # }
+
+  title.capitalize.split.each { |word| 
+    exceptions.downcase.split.include?(word) ? finished << word : finished << word.capitalize
+  }
+
+
+
+  finished.join(' ')
 
 end
--- title_case/spec/title_case_spec.rb
+++ title_case/spec/title_case_spec.rb
@@ -1,10 +1,30 @@
 require 'title_case'
 
-describe '.title_case' do 
-  it 'capitlizes the first word' do
-    expect(title_case('foo', '')).to eq 'Foo'
+describe '.title_case' do
+
+  it 'capitalizes two words' do
+    expect(title_case('foo bar')).to eq 'Foo Bar'
   end
 
+  it 'does not capitalize one exception' do
+    expect(title_case('foo bar', 'bar')).to eq 'Foo bar'
+  end
+
+  it 'works with a longer title string' do
+    expect(title_case('foo bar hello world yes hi hello', 'hello')).to eq 'Foo Bar hello World Yes Hi hello'
+  end
+
+  it 'works with a longer title and over 1 exception' do
+    expect(title_case('foo bar hello world yes hi hello', 'bar world yes')).to eq 'Foo bar Hello world yes Hi Hello'
+  end
+
+  it 'capitalizes the first word even when given an exception' do
+    expect(title_case('foo bar', 'foo')).to eq 'Foo Bar'
+  end 
+
+  it 'doent exclude a word if it is inside another word' do
+    expect(title_case('bar foo','food')).to eq 'Bar Foo' 
+  end
 
 
 end
