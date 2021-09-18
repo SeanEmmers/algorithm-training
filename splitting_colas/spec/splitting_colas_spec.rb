edec46f83c9886ce920bac8adbc55fc90f54e349
Sean Phillips <seanphillips9696@gmail.com>
2021-09-18 14:57:57 +0200

    failing first test

---
+++ splitting_colas/spec/splitting_colas_spec.rb
@@ -0,0 +1,9 @@
+require 'splitting_colas'
+
+describe '.splitting_colas' do
+  it 'returns the first name in the array when the number is one' do
+    expect(splitting_colas(['Sean'], 1)).to eq 'Sean'
+  end 
+
+
+end
