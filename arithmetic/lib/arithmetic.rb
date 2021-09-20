5efad3b21a3f6ae045fa74bdf487b3400ea471d2
Sean Phillips <seanphillips9696@gmail.com>
2021-09-20 19:30:40 +0200

    tests passing

--- arithmetic/lib/arithmetic.rb
+++ arithmetic/lib/arithmetic.rb
@@ -4,5 +4,6 @@ def arithmetic(num1, num2, action)
   return num1 + num2 if action == 'add'
   return num1 - num2 if action == 'subtract'
   return num1 * num2 if action == 'multiply'
+  return num1 / num2 if action == 'divide'
 
 end
