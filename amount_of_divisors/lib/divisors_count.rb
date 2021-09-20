4dd99b16a6af343e1540589f43fa270677b81ee3
Sean Phillips <seanphillips9696@gmail.com>
2021-09-20 19:08:09 +0200

    passing first test

--- amount_of_divisors/lib/divisors_count.rb
+++ amount_of_divisors/lib/divisors_count.rb
@@ -1,4 +1,9 @@
 def divisors_count(num)
   count = 0
+  
+  (0..num).each { |number|
+    count += 1 if number % 2 == 0 
+  }
 
+  count
 end
--- amount_of_divisors/spec/divisors_count_spec.rb
+++ amount_of_divisors/spec/divisors_count_spec.rb
@@ -1,8 +1,8 @@
 require 'divisors_count'
 
-describe '.devisors_count' do
+describe '.divisors_count' do
   it 'retuns the number of times of a single divisor' do
-    expect(devisors_count(1)).to eq 1
+    expect(divisors_count(1)).to eq 1
   end
 
 
