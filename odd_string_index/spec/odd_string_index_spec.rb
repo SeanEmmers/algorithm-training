5ecc8eea9f633e38b53a79eb2bab9be8983f680e
Sean Phillips <seanphillips9696@gmail.com>
2021-09-21 15:40:57 +0200

    edge case test

--- odd_string_index/spec/odd_string_index_spec.rb
+++ odd_string_index/spec/odd_string_index_spec.rb
@@ -10,4 +10,7 @@ describe 'odd_string_index' do
     expect(odd_string_index('foo bar')).to eq 'FoO BaR'
   end
 
+  it 'works with words that have random capitals in them' do
+    expect(odd_string_index('fOO BAR hElLo WORld')).to eq 'FoO BaR HeLlO WoRlD'
+  end
 end
