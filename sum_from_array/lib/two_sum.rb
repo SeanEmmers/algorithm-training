422c582f83b84ec4b4214d4b786300e9808cf212
Sean Phillips <seanphillips9696@gmail.com>
2021-09-21 10:20:59 +0200

    failing first test

--- sum_from_array/lib/two_sum.rb
+++ sum_from_array/lib/two_sum.rb
@@ -1,4 +1,19 @@
 def two_sum(arr, target)
-  
+
+  key = 1
+  final = []
+
+  arr.each { |num| 
+    if 
+      num + arr[key] == target
+      key += 1
+      final << arr[num]
+      final << arr[key]
+    end
+  }
+
+  final
+
+  arr.length
 
 end
--- sum_from_array/spec/two_sum_spec.rb
+++ sum_from_array/spec/two_sum_spec.rb
@@ -1,9 +1,9 @@
 require 'two_sum'
 
 describe '.two_sum' do
+
   it 'find the sum of two' do
     expect(two_sum([1,1,2], 2)).to eq [0,1]
   end
-
 
 end
