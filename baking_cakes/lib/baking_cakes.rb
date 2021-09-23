fd6377c1743e0ae5752b450670ddb96d05544c42
Sean Phillips <seanphillips9696@gmail.com>
2021-09-23 12:16:13 +0200

    first test passing

--- baking_cakes/lib/baking_cakes.rb
+++ baking_cakes/lib/baking_cakes.rb
@@ -1,3 +1,13 @@
 def baking_cakes(recipe, ingredients)
 
+  weights = []
+
+  ingredients.each do |ingredient, weight|
+    weights << (weight / recipe[ingredient]) if recipe.include?(ingredient)
+  end
+
+  weights.sort!
+  weights[0]
+  
+
 end
