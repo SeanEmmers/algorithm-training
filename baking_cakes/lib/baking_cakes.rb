6f209c23e715d2a517b755446823b7c0ff693c52
Sean Phillips <seanphillips9696@gmail.com>
2021-09-23 12:34:39 +0200

    second test passing

--- baking_cakes/lib/baking_cakes.rb
+++ baking_cakes/lib/baking_cakes.rb
@@ -6,8 +6,10 @@ def baking_cakes(recipe, ingredients)
     weights << (weight / recipe[ingredient]) if recipe.include?(ingredient)
   end
 
+  recipe.each_key { |ingredient| weights << 0 unless ingredients.include?(ingredient) }
+
   weights.sort!
   weights[0]
-  
+
 
 end
--- baking_cakes/spec/baking_cakes_spec.rb
+++ baking_cakes/spec/baking_cakes_spec.rb
@@ -1,8 +1,12 @@
 require 'baking_cakes'
 
 describe '.baking_cakes' do
-  it 'works' do
+  it 'works with an even match' do
     expect(baking_cakes({"flour"=>500, "sugar"=>200, "eggs"=>1},{"flour"=>1200, "sugar"=>1200, "eggs"=>5, "milk"=>200})).to eq 2
+  end
+
+  it 'returns zero when there is an ingredient missing' do
+    expect(baking_cakes({"flour"=>500, "sugar"=>200, "eggs"=>1},{"flour"=>1200, "sugar"=>1200})).to eq 0
   end
 
 
