8be5bcc38936584cacc441ad2efb4295fe084283
Sean Phillips <seanphillips9696@gmail.com>
2021-09-24 13:19:22 +0200

    failing test for draw result

--- good_vs_evil/spec/good_vs_evil_spec.rb
+++ good_vs_evil/spec/good_vs_evil_spec.rb
@@ -8,7 +8,11 @@ describe 'good_vs_evil' do
   it 'returns the correct string when evil wins' do
     expect(good_vs_evil('0 1 0 0 0 1', '0 1 1 1 1 1 0')).to eq "Battle Result: Evil eradicates all trace of Good"
   end
-  
+
+  it 'returns the correct string in the result of a draw' do 
+    expect(good_vs_evil('0 1 0 0 1 1', '0 1 0 0 1 1')).to eq "Battle Result: No victor on this battle field"
+  end
+
 
 
 
