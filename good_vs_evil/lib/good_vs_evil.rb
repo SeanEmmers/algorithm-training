654d1d3039123a39f9ca480e106ff513516baff4
Sean Phillips <seanphillips9696@gmail.com>
2021-09-24 13:19:54 +0200

    tie test passing

--- good_vs_evil/lib/good_vs_evil.rb
+++ good_vs_evil/lib/good_vs_evil.rb
@@ -7,8 +7,8 @@ def good_vs_evil(good_army, evil_army)
   evil_army.split.map! { |i| evil_count += i.to_i }
 
   return "Battle Result: Good triumphs over Evil" if good_count > evil_count
-
   return "Battle Result: Evil eradicates all trace of Good" if evil_count > good_count
+  return "Battle Result: No victor on this battle field" if evil_count == good_count
 
   
 end
