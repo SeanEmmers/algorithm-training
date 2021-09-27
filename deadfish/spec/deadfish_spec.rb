28a25d8e871cc9b83610b1332a84f344f2176e6b
Sean Phillips <seanphillips9696@gmail.com>
2021-09-27 14:05:22 +0200

    multiple o test

--- deadfish/spec/deadfish_spec.rb
+++ deadfish/spec/deadfish_spec.rb
@@ -8,7 +8,11 @@ describe '.deadfish' do
   it 'increments the value by one if i is present' do
     expect(deadfish('io')).to eq [1]
   end
-  
+
+  it "works with multiple o's " do
+    expect(deadfish('ooo')).to eq [0,0,0]
+  end
+
 
 
 end
