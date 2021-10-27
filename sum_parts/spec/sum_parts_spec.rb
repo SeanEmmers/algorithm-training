a61c3c986c630a3fc7b26ce58df5f1128f474a9f
Sean Phillips <seanphillips9696@gmail.com>
2021-10-27 12:24:14 +0100

    second test failing

--- sum_parts/spec/sum_parts_spec.rb
+++ sum_parts/spec/sum_parts_spec.rb
@@ -4,6 +4,10 @@ describe 'sum_parts' do
   it 'returns zero as the last element of the array' do
     expect(sum_parts([])).to eq [0]
   end
+  
+  it 'returns the first element' do
+    expect(sum_parts([10])).to eq [10, 0]
+  end
 
-
+  
 end
